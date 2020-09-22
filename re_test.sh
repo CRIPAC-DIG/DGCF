78ff58e7deab14e51fc5716cff8bb01ea9696444
ZM7 <1447402598@qq.com>
2020-09-22 09:57:45 +0800

    Delete re_test.sh

--- re_test.sh
+++
@@ -1,9 +0,0 @@
-#!/user/bin/env/ bash
-CUDA_VISIBLE_DEVICES=5 nohup  python2 -u  evaluate_interaction_prediction.py \
- --network reddit \
- --epoch 30 \
- --span_num 1000 \
- --adj \
- --method attention \
- >./results/reddit_attention_30&
-
