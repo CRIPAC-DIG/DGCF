c7f018eaab57d4ca781ceccaf6136c85c75e3203
ZM7 <1447402598@qq.com>
2020-09-22 09:58:22 +0800

    Delete wi_test.sh

--- wi_test.sh
+++
@@ -1,9 +0,0 @@
-#!/user/bin/env/ bash
-CUDA_VISIBLE_DEVICES=5 nohup  python2 -u  evaluate_interaction_prediction.py \
- --network wikipedia \
- --epoch 48 \
- --method attention \
- --adj \
- --length 120 \
- >./results/wiki_neig120_48_test&
-
